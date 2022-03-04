8cae8b26b73fd41daaaeecd80f786fca61f33902
t0mcr8se <husamirami@gmail.com>
2022-03-05 02:09:11 +0300

    add/fix more vesting/multicall hooks

--- src/hooks/useVesting.jsx
+++ src/hooks/useVesting.jsx
@@ -1,32 +1,33 @@
 import { useMemo, useState } from 'react'
 import { TOKENSWAP_VESTING_ADDRESSES } from '../constants'
+// import BigNumber from 'bignumber.js'
 import {
   useVestingContract,
   useMultipleContractsSingleInterface,
 } from './useContract'
-import { useMultiCallSameData, useSingleCallResult } from './useMultiCall'
+import { useMultiCallSameData, useSingleCallMultiData, useSingleCallResult } from './useMultiCall'
 import { useWeb3Context } from '@/context/web3'
 import VESTING_ABI from '@/constants/abis/vesting.json'
 
 export function useDecodeSolidityUintArrays(bytesArray) {
-  // bytesArray: ['0x00..00': EncodedArray, '0x00..00': EncodedArray, ...]
-  // returns [['1','2','3'], ['3'], ...]
-  const { web3 } = useWeb3Context()
+  return useWeb3DecodeParameters(bytesArray, ['uint256[]'])
+}
+
+function useWeb3DecodeParameters(bytesArray, argTypes){
+  const {web3} = useWeb3Context()
   return useMemo(() => {
-    return bytesArray.map((bytes) =>
-      web3.eth.abi.decodeParameter('uint256[]', bytes),
-    )
-  }, [web3, bytesArray])
+    return bytesArray.map(bytes => web3.eth.abi.decodeParameters(argTypes, bytes))
+  }, [web3, bytesArray, argTypes])
 }
 
 export function useVestingIds(vestingAddress) {
   const { account } = useWeb3Context()
   const vestingContract = useVestingContract(vestingAddress)
 
-  const vestingIds = useSingleCallResult(vestingContract, 'getActiveGrants', [
+  const rawResult = useSingleCallResult(vestingContract, 'getActiveGrants', [
     account,
   ])
-  return vestingIds
+  return useDecodeSolidityUintArrays(rawResult?.returnData ??[])[0]
 }
 
 export function useAllVestingIds() {
@@ -45,92 +46,35 @@ export function useAllVestingIds() {
   return results
 }
 
-// export function useUnclaimedAmount(vestingAddress, grantId){
-//   const {account} = useWeb3Context()
-//   const vestingContract = useVestingContract(vestingAddress)
-//   const [claims, setClaims] = useState(0)
 
-//   useAsyncWeb3Call(
-//     vestingContract.methods.calculateGrantClaim(grantId).call(),
-//     setClaims
-//   )
-// }
+export function useClaims(vestingAddress){
+  const vestingContract = useVestingContract(vestingAddress)
+  const vestingIds = useVestingIds(vestingAddress)
+  const args = useMemo(() => {
+    if(!vestingIds) return
 
-// export function useVestingIds(vestingAddress, account) {
-//   const vestingContract = useVestingContract(vestingAddress)
-//   console.log(vestingContract)
-//   const claimIdsResult = useSingleCallResult(vestingContract, 'getActiveGrants', [account])
+    return vestingIds[0]?.map(id => [id])
+  }, [vestingIds])
 
-//   return useMemo(() => {
-//     if(!claimIdsResult) return
-//     return claimIdsResult.result?.[0]
-//   }, [claimIdsResult?.result])
-// }
+  const rawResult = useSingleCallMultiData(vestingContract, 'calculateGrantClaim', args)
 
-// export function useVestingClaims(vestingAddress, account) {
-//   const vestingIds = useVestingIds(vestingAddress, account)
-//   const vestingContract = useVestingContract(vestingAddress)
-//   const userClaims = useSingleContractMultipleData(
-//     vestingContract,
-//     'tokenGrants',
-//     vestingIds ? vestingIds.map((id) => [id]) : undefined
-//   )
+  const claims = useWeb3DecodeParameters(rawResult?.returnData ?? [], ['uint16', 'uint256'])
+  return claims
+}
 
-//   return useMemo(
-//     () =>
-//       vestingIds &&
-//       vestingIds.reduce((memo, vestingId, index) => {
-//         memo[vestingId] = userClaims?.[index]?.result
-//         return memo
-//       }, {}),
-//     [userClaims, vestingIds]
-//   )
-// }
 
-// export function useVestingUnclaimedAmounts(vestingAddress, account) {
-//   const vestingContract = useVestingContract(vestingAddress)
-//   console.log({vestingContract, account})
-//   const vestingIds = useVestingIds(vestingAddress, account)
-//   const unclaimedAmounts = useSingleContractMultipleData(
-//     vestingContract,
-//     'calculateGrantClaim',
-//     vestingIds ? vestingIds.map((id) => [id]) : undefined
-//   )
-//   console.log("giusgoiu")
-//   console.log(vestingIds)
-//   console.log(vestingContract)
-//   console.log(unclaimedAmounts)
+export function useTotalClaim(vestingAddress){
+  const claims = useClaims(vestingAddress)
 
-//   return useMemo(
-//     () =>
-//       vestingIds
-//         ? vestingIds.reduce((memo, vestingId, index) => {
-//             memo[vestingId] = unclaimedAmounts[index]?.result?.[1]
-//             return memo
-//           }, {})
-//         : {},
-//     [unclaimedAmounts, vestingIds]
-//   )
-// }
+  return useMemo(() => {
+    claims.reduce((i, sum) => (new BigNumber(claims[i][1])).plus(sum), newBigNumber('0'))
+  })
+}
 
-// export function useVestingTotalUnclaimedAmounts(vestingAddress, account) {
-//   const unclaimedAmounts = useVestingUnclaimedAmounts(vestingAddress, account)
 
-//   return useMemo(() => {
-//     return Object.values(unclaimedAmounts).reduce((memo, amount) => memo?.add(amount ?? 0), BigNumber.from(0))
-//   }, [unclaimedAmounts])
-// }
-
-// export function useVestingClaimableIds(vestingAddress, account) {
-//   const unclaimedAmounts = useVestingUnclaimedAmounts(vestingAddress, account)
-
-//   return Object.keys(unclaimedAmounts)
-//     .map((key) => (unclaimedAmounts?.[key]?.gt(0) ? key : undefined))
-//     .filter(Boolean)
-// }
-
-// export function useVestingTotalUnclaimedAmount() {
-//   const { account } = useActiveWeb3React()
-//   const [swapAClaimableAmount] = [useVestingTotalUnclaimedAmounts(TOKENSWAP_VESTING_ADDRESSES['0.03'], account ?? undefined)]
-//   return swapAClaimableAmount
-// }
+export function useClaim(vestingAddress, grantId){
+  const vestingContract = useVestingContract(vestingAddress)
+  const rawResult = useSingleCallResult(vestingContract, 'calculateGrantClaim', [grantId])
+  const claims = useWeb3DecodeParameters(rawResult?.returnData ?? [], ['uint16', 'uint256'])
+  return claims
+}
