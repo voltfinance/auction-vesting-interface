3fb3fd80f126d30435f0da1bd7b6ae48b493925b
RudeWolfCoding <51991885+RudeWolfCoding@users.noreply.github.com>
2022-03-07 23:21:05 +0100

    Update header.jsx

--- src/components/home/header.jsx
+++ src/components/home/header.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
-import wallet from '@/assets/images/wallet.svg'
-import { useWeb3Context } from '@/context/web3'
-import useSwitchNetwork from '@/hooks/useSwitchNetwork'
-import useFuseBalance from '@/hooks/useFuseBalance'
 import voltage from '@/assets/images/voltage_icon.png'
-import { fromWei } from '@/utils/number'
 import {useNavigate} from 'react-router-dom'
 
 function Header () {
   const navigate = useNavigate()
-  const { account, toggleWeb3Modal, chainId } = useWeb3Context()
-  const switchNetwork = useSwitchNetwork()
-  const fuseBalance = fromWei(useFuseBalance(account))
 
   return (
     <div className='header'>
-      <img className='icon' alt='voltage' src={voltage} onClick={() => {navigate('/')}}/>
+      <img style={{cursor: 'pointer'}} className='icon' alt='voltage' src={voltage} onClick={() => {navigate('/')}}/>
     </div>
   )
 }
