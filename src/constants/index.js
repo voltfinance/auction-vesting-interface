a2736059a387e5f2312d1f9d6fb947c2b33fab97
t0mcr8se <husamirami@gmail.com>
2022-03-09 16:00:45 +0300

    add check for private sales

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,24 +9,28 @@ export const TOKEN_SWAP_CONTRACTS = {
     name: "Ecosystem Round 0.003",
     address: "0x9B2BC0df082BdAD4C2FeaB73704a6EAe304a8A0e",
     tokenSale: "0x341D19E4e0c3C1b009F6Af46324B0128F91231cA",
+    isPrivate: false,
     isSingleVesting: false
   },
   "0x763c3d72dbdeb498D3dc196B68c9fb8e1d7B61b3": {
     name: "Ecosystem Round 0.0035",
     address: "0x763c3d72dbdeb498D3dc196B68c9fb8e1d7B61b3",
     tokenSale: "0x4E693E8CfFCF7AA7E149C9D54690a1EB6ef3952F",
+    isPrivate: false,
     isSingleVesting: false
   },
   "0xBFDE08aB662B907146c78077310E4c2fFaCD302E": {
     name: "Ecosystem Round 0.004",
     address: "0xBFDE08aB662B907146c78077310E4c2fFaCD302E",
     tokenSale: "0x4D303E6bFB675588e4c6928ecf65Ca80723Bf394",
+    isPrivate: false,
     isSingleVesting: false
   },
   "0xEE7E908e5B2667eA5c7E82e4d69cd755f74594AC": {
     name: "Ecosystem Round 0.006",
     address: "0xEE7E908e5B2667eA5c7E82e4d69cd755f74594AC",
     tokenSale: "0x6Eb6a038741FeDc23fE67f0727ba43D15D147958",
+    isPrivate: false,
     isSingleVesting: true
   },
 
